1d95d15d49c12439d1d81395bd32a5ef1365919c
wahid-ahmed <wahidahmed1992@gmail.com>
2022-08-23 16:15:35 +0600

    154 pending

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -47,6 +47,11 @@ string[] words = { "gin", "zen", "gig", "msg" };
 //int[] nums = { 4, 5, 6, 7, 0, 1, 2 };
 //Console.WriteLine(LeetCode.FindMin_153(nums));
 #endregion #region 153. Find Minimum in Rotated Sorted Array
+
+#region 154. Find Minimum in Rotated Sorted Array II
+//int[] nums = { 4, 5, 6, 7, 0, 1, 2 };
+//Console.WriteLine(LeetCode.FindMin_154(nums));
+#endregion 154. Find Minimum in Rotated Sorted Array II
 public static class LeetCode
 {
     #region 120. Triangle
@@ -310,5 +315,60 @@ public static class LeetCode
         return 0;
     }
     #endregion 153. Find Minimum in Rotated Sorted Array
+
+    #region 154. Find Minimum in Rotated Sorted Array II
+
+    //1.if the array is pivoted (n-1) times then the end value must be less or equal to start value.
+    //2.if the array is pivoted n times then the start value must be the minimum value.In that case,the start value must be less than end value
+    //3. *//
+    public static int FindMin_154(int[] nums)
+    {
+        int lIndex = 0;
+        int lElement = nums[lIndex];
+        int rIndex = nums.Length - 1;
+        int rElement = nums[rIndex];
+        bool nTimeRoteted = true;
+        if (nums.Length == 0) return nums[0];
+        if (nums[0] >= nums[nums.Length - 1])
+        {
+            nTimeRoteted = false;
+        }
+
+        if (!nTimeRoteted)
+        {
+            while (lIndex <= rIndex)
+            {
+                int mIndex = lIndex + (rIndex - lIndex) / 2;
+                int mElement = nums[mIndex];
+                if (mIndex>0 && mIndex<nums.Length-1)
+                {
+                    if (mElement < nums[mIndex-1] && mElement < nums[mIndex + 1])
+                    {
+                        return nums[mIndex];
+                    }
+                    else if (mElement >= nums[mIndex - 1] && mElement>lElement)
+                    {
+                        lIndex = mIndex + 1;
+                    }
+                }
+                if (mElement>lElement && mElement>rElement)
+                {
+                    lIndex = mIndex + 1;
+                }
+                else if (mElement < lElement && mElement < rElement)
+                {
+                    rIndex = mIndex-1;
+                }
+            }
+            return 0;
+        }
+        else
+        {
+            return nums[0];
+        }
+
+      
+    }
+    #endregion 154. Find Minimum in Rotated Sorted Array II
 }
 
